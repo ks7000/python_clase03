ece45bf0b011aa7a38775380fbcf205db7e202d7
Jimmy_Olano <olano@ks7000.net.ve>
2014-10-17 11:07:03 -0430

    Agrego 'click.pocoo.rg' a 'correr_metodos_estaticos.py'

--- correr_metodos_estaticos.py
+++ correr_metodos_estaticos.py
@@ -1,4 +1,13 @@
 # -*- coding:utf-8 -*-
+import click
 from metodos_estaticos import MiClase
-a = MiClase()
-a.metodo_estatico(" el ejemplo en sí de metodos estaticos")
+
+@click.command()
+@click.option( '--mensaje', default=' el ejemplo en sí de metodos estaticos', prompt='Introduzca un mensaje diferente', help='Permite cambiar el mensaje por defecto a mostrar')
+def correr_estatico(mensaje):
+    a = MiClase()
+    #a.metodo_estatico(" el ejemplo en sí de metodos estaticos")
+    a.metodo_estatico(mensaje)
+    
+if __name__ == '__main__':
+    correr_estatico()
